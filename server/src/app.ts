c87226a7b7efc6ba0a7887a2489d0948ca451a40
Co9xs <ryo.fjsm0203@icloud.com>
2022-01-07 11:31:30 +0900

    feat: update dependencies used for check jwt

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import session from 'express-session';
 
+import { allowCrossDomain } from './middlewares/allowCrossDomain';
 import { checkJwt } from './middlewares/checkJwt';
 import { apiRouter } from './routes/api';
 import { staticRouter } from './routes/static';
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     proxy: true,
@@ -15,6 +17,7 @@ app.use(
     secret: 'secret',
   })
 );
+app.use(allowCrossDomain);
 app.use(checkJwt);
 
 app.use('/api/v1', apiRouter);
