36bc01ff2233864bea1ea2ef0f11198b49711d55
Co9xs <ryo.fjsm0203@icloud.com>
2022-01-07 16:26:46 +0900

    feat: create accountifo component

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,4 +1,11 @@
-export type User = {
+export type User = Readonly<{
   id: number;
   auth0Id: string;
-};
+}>;
+
+export type Account = Readonly<{
+  id: number;
+  name: string;
+  balance: number;
+  ownerId: number;
+}>;
