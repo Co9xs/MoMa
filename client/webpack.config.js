8f6e1d84410e2b7655d0d4aa5063e8ec1f3b2de1
Co9xs <ryo.fjsm0203@icloud.com>
2022-01-10 13:59:50 +0900

    feat: setup scratch react project

---
+++ client/webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const SRC_PATH = path.resolve(__dirname, "./src");
+const PUBLIC_PATH = path.resolve(__dirname, "../public");
+const DIST_PATH = path.resolve(__dirname, "./dist");
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  devServer: {
+    historyApiFallback: true,
+    host: "0.0.0.0",
+    port: 3000,
+    static: [PUBLIC_PATH],
+  },
+  entry: {
+    style: path.resolve(SRC_PATH, "./index.css"),
+    main: path.resolve(SRC_PATH, "./index.tsx"),
+  },
+  output: {
+    path: DIST_PATH,
+    filename: "scripts/[name].js",
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.css$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader", options: { url: false } },
+          { loader: "postcss-loader" },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles/[name].css",
+    }),
+    new RemoveEmptyScriptsPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(SRC_PATH, "./index.html"),
+      publicPath: "/",
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+  target: ["web", "es5"],
+};
