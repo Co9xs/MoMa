1ff2d39036d60ed9ddf936f78272db161908a8c7
Co9xs <ryo.fjsm0203@icloud.com>
2022-01-11 15:26:50 +0900

    setting eslint in server directory and fix lint error

--- server/src/routes/api/credit_card_statement.ts
+++ server/src/routes/api/credit_card_statement.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import Router from 'express-promise-router';
 import httpErrors from 'http-errors';
 import { PrismaClient } from '@prisma/client';
