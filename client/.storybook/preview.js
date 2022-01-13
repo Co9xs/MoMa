be86d6449ffdd84174673c6d6f52d80b3af42f2e
Co9xs <ryo.fjsm0203@icloud.com>
2022-01-13 11:54:51 +0900

    feat: create navigation item component

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,6 +1,13 @@
-import "../src/index.css";
+import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router';
+
+import '../src/index.css';
+
+// add decorator for rendering component using react router packages
+addDecorator((story) => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>);
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
