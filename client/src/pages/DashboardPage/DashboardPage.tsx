6e2e2146bd3e3e03e359be2c270d9230938c2ddc
Co9xs <ryo.fjsm0203@icloud.com>
2022-01-15 11:57:11 +0900

    feat: create dashboard page container

--- client/src/pages/DashboardPage/DashboardPage.tsx
+++ client/src/pages/DashboardPage/DashboardPage.tsx
@@ -1,16 +1,11 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
 import { AccountList } from '@components/AccountList';
 import { CreditCardList } from '@components/CreditCardList';
-import { MainLayout } from '@components/MainLayout';
-import { Navigation } from '@components/Navigation';
 
 import { Account, CreditCard } from '../../types';
 
 interface Props {
   accounts: Account[];
   creditCards: CreditCard[];
-  onRequestLogin: () => void;
-  onRequestLogout: () => void;
   onRequestOpenAccountModal: () => void;
   onRequestOpenCreditCardModal: () => void;
 }
@@ -18,19 +13,14 @@ interface Props {
 const DashboardPage: React.VFC<Props> = ({
   accounts,
   creditCards,
-  onRequestLogin,
-  onRequestLogout,
   onRequestOpenAccountModal,
   onRequestOpenCreditCardModal,
 }) => {
   return (
-    <MainLayout>
-      <Navigation activeUser={null} onRequestLogin={onRequestLogin} onRequestLogout={onRequestLogout} />
-      <div className='h-full bg-moma-20 grid grid-flow-col grid-col-2 gap-4 p-4'>
-        <AccountList accounts={accounts} onRequestOpenAccountModal={onRequestOpenAccountModal} />
-        <CreditCardList creditCards={creditCards} onRequestOpenCreditCardModal={onRequestOpenCreditCardModal} />
-      </div>
-    </MainLayout>
+    <div className='h-full bg-moma-20 grid grid-flow-col grid-col-2 gap-4 p-4'>
+      <AccountList accounts={accounts} onRequestOpenAccountModal={onRequestOpenAccountModal} />
+      <CreditCardList creditCards={creditCards} onRequestOpenCreditCardModal={onRequestOpenCreditCardModal} />
+    </div>
   );
 };
 
