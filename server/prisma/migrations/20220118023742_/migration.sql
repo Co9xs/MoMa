41823ed45352ada4ba74ff75dfbbb3b583cc8e54
Co9xs <ryo.fjsm0203@icloud.com>
2022-01-18 11:38:11 +0900

    feat: update migration

--- server/prisma/migrations/20220118023742_/migration.sql
+++ server/prisma/migrations/20220118023742_/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "Month" AS ENUM ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
@@ -11,7 +14,7 @@ CREATE TABLE "Subscription" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
-    "subscriberId" INTEGER NOT NULL,
+    "subscriberId" TEXT NOT NULL,
 
     CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
 );
@@ -21,7 +24,7 @@ CREATE TABLE "CreditCard" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "budget" INTEGER NOT NULL,
-    "ownerId" INTEGER NOT NULL,
+    "ownerId" TEXT NOT NULL,
 
     CONSTRAINT "CreditCard_pkey" PRIMARY KEY ("id")
 );
@@ -30,6 +33,8 @@ CREATE TABLE "CreditCard" (
 CREATE TABLE "CreditCardStatement" (
     "id" SERIAL NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
+    "year" TEXT NOT NULL,
+    "month" "Month" NOT NULL,
     "name" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
     "creditCardId" INTEGER NOT NULL,
@@ -42,7 +47,7 @@ CREATE TABLE "Account" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "balance" INTEGER NOT NULL,
-    "ownerId" INTEGER NOT NULL,
+    "ownerId" TEXT NOT NULL,
 
     CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
 );
@@ -51,6 +56,8 @@ CREATE TABLE "Account" (
 CREATE TABLE "AccountStatement" (
     "id" SERIAL NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
+    "year" TEXT NOT NULL,
+    "month" "Month" NOT NULL,
     "name" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
     "accountId" INTEGER NOT NULL,
@@ -58,47 +65,20 @@ CREATE TABLE "AccountStatement" (
     CONSTRAINT "AccountStatement_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "Wallet" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "balance" INTEGER NOT NULL,
-    "ownerId" INTEGER NOT NULL,
-
-    CONSTRAINT "Wallet_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "WalletStatement" (
-    "id" SERIAL NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
-    "name" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "walletId" INTEGER NOT NULL,
-
-    CONSTRAINT "WalletStatement_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "User_auth0Id_key" ON "User"("auth0Id");
 
 -- AddForeignKey
-ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_subscriberId_fkey" FOREIGN KEY ("subscriberId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_subscriberId_fkey" FOREIGN KEY ("subscriberId") REFERENCES "User"("auth0Id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "CreditCard" ADD CONSTRAINT "CreditCard_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "CreditCard" ADD CONSTRAINT "CreditCard_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("auth0Id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "CreditCardStatement" ADD CONSTRAINT "CreditCardStatement_creditCardId_fkey" FOREIGN KEY ("creditCardId") REFERENCES "CreditCard"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Account" ADD CONSTRAINT "Account_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Account" ADD CONSTRAINT "Account_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("auth0Id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "AccountStatement" ADD CONSTRAINT "AccountStatement_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Wallet" ADD CONSTRAINT "Wallet_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "WalletStatement" ADD CONSTRAINT "WalletStatement_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallet"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
