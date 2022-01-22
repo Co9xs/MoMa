3234af6ad31897003d0768cdcdfc2583c9031e55
Co9xs <ryo.fjsm0203@icloud.com>
2022-01-22 11:48:50 +0900

    feat: enabled session

--- client/src/containers/AppPageContainer/AppPageContainer.tsx
+++ client/src/containers/AppPageContainer/AppPageContainer.tsx
@@ -25,8 +25,8 @@ const AppPageContainer: React.VFC = () => {
 
   return (
     <>
-      {<div>{JSON.stringify(data.auth0Id)}</div>}
-      <AppPage activeUserId={data.auth0Id} onRequestLogin={loginWithRedirect} onRequestLogout={logout}>
+      {<div>{JSON.stringify(data)}</div>}
+      <AppPage activeUserId={null} onRequestLogin={loginWithRedirect} onRequestLogout={logout}>
         <Routes>
           <Route element={<DashboardPageContainer />} path='/' />
           <Route element={<NotFoundPageContainer />} path='*' />
