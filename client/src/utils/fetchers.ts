546f5c1a66cd55c1ea4d0a495d70816d12ac1c6d
Co9xs <ryo.fjsm0203@icloud.com>
2022-01-25 10:23:18 +0900

    feat: add get credit card list method

--- client/src/utils/fetchers.ts
+++ client/src/utils/fetchers.ts
@@ -1,5 +1,5 @@
 import { User } from '@auth0/auth0-react';
-import { Account } from 'src/types';
+import { Account, CreditCard } from 'src/types';
 
 const setSession: (user: User) => Promise<{ auth0Id: string }> = async (user) => {
   const res = await fetch(`/api/v1/signin`, {
@@ -36,4 +36,22 @@ const getAccountList: (accessToken: string) => Promise<Account[]> = async (acces
   return res.json() as Promise<Account[]>;
 };
 
-export { setSession, getAccountList };
+const getCreditCardList: (accessToken: string) => Promise<CreditCard[]> = async (accessToken) => {
+  const res = await fetch('/api/v1/accounts', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    mode: 'cors',
+    credentials: 'include',
+  });
+
+  if (!res.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  return res.json() as Promise<CreditCard[]>;
+};
+
+export { setSession, getAccountList, getCreditCardList };
