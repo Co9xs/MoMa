f14346c54bbe32cc0ae26741f2b597f00ebd855b
Co9xs <ryo.fjsm0203@icloud.com>
2022-01-27 11:20:14 +0900

    feat: improve ui

--- client/src/components/CreditCardItem/CreditCardItem.tsx
+++ client/src/components/CreditCardItem/CreditCardItem.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const CreditCardItem: React.VFC<Props> = ({ creditCard }) => {
   return (
-    <div className='p-3 shadow bg-moma-10'>
+    <div className='p-3 shadow bg-moma-10 rounded-md'>
       <h3 className='text-xl text-moma-80 font-bold'>{creditCard.name}</h3>
       <div className='pt-2 px-2'>
         <p className='text-moma-70'>今月の予算</p>
